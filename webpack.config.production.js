511e8e867e06d8abf13424bc354de87b7309fe27
Chris McKnight <cmckni3@gmail.com>
2018-05-28 16:15:25 -0500

    Add spectacle

---
+++ webpack.config.production.js
@@ -0,0 +1,57 @@
+/* eslint-disable */
+
+var path = require("path");
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var BASE_HREF = '/';
+
+if (process.env.BASE_HREF) {
+  BASE_HREF = process.env.BASE_HREF;
+}
+
+module.exports = {
+  entry: [
+    "babel-polyfill",
+    "./index"
+  ],
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        "NODE_ENV": JSON.stringify("production")
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    }),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      baseUrl: BASE_HREF
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.md$/,
+      loader: "html-loader!markdown-loader?gfm=false"
+    }, {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: "babel-loader"
+    }, {
+      test: /\.css$/,
+      loader: "style-loader!css-loader"
+    }, {
+      test: /\.(png|jpg|gif)$/,
+      loader: "url-loader?limit=8192"
+    }, {
+      test: /\.svg$/,
+      loader: "url-loader?limit=10000&mimetype=image/svg+xml"
+    }]
+  }
+};
